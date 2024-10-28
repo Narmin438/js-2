b275cb8971d451e4499625ee3dbccf37b54515a1
Narmin438 <bnermin2009@gmail.com>
2024-10-28 18:55:22 +0400

    js-2

---
+++ task.js
@@ -0,0 +1,26 @@
+// task-3
+
+let firstName = "Narmin";
+let lastName = "Babayeva";
+console.log(`Sizi bir daha görməyimizə şadıq, ${firstName + " " + lastName}. `);
+
+
+// task-4
+
+let fullName = "Narmin Babayeva";
+fullName = fullName.split(" ");
+console.log(fullName[0].length + fullName[1].length);
+
+
+// task-5
+
+let fullName1 = "Narmin Babayeva"
+console.log(fullName1.split(" ")[0][0]);
+console.log(fullName1.split(" ")[1][0]);
+
+
+// task-6
+
+let fullName2 = "Narmin Babayeva"
+fullName2 = fullName2.split(" ")
+console.log(fullName2[1] + " " + fullName2[0]);
